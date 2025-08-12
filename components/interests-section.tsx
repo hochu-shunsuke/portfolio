31e0272924687063a81d87d2ffd0e01e77a04407
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2025-08-12 09:12:12 +0900

    プレイリストの並び変更

--- components/interests-section.tsx
+++ components/interests-section.tsx
@@ -10,10 +10,10 @@ const musicPlaylists = [
     spotifyUrl: "https://open.spotify.com/embed/playlist/6BLQf0A5yzSJVAI5Wo1ECC?utm_source=generator&theme=0"
   },
   {
-    spotifyUrl: "https://open.spotify.com/embed/playlist/6qeI7Fjsyrhet1ba8x3B0F?utm_source=generator&theme=0"
+    spotifyUrl: "https://open.spotify.com/embed/playlist/1eNiVtQ6RWz2XqDDOYmTyW?utm_source=generator&theme=0"
   },
   {
-    spotifyUrl: "https://open.spotify.com/embed/playlist/1eNiVtQ6RWz2XqDDOYmTyW?utm_source=generator&theme=0"
+    spotifyUrl: "https://open.spotify.com/embed/playlist/6qeI7Fjsyrhet1ba8x3B0F?utm_source=generator&theme=0"
   },
   {
     spotifyUrl: "https://open.spotify.com/embed/playlist/4jjhdyoD1NymbcLZBMCVkE?utm_source=generator&theme=0"
