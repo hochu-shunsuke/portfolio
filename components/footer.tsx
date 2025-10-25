cd577a5503e5498558be41188c01830dea10865f
hochu-shunsuke <124444636+hochu-shunsuke@users.noreply.github.com>
2025-10-25 19:56:40 +0900

    footer.tsx の更新

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,9 @@ export function Footer() {
     <footer className="border-t border-zinc-800 py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row md:py-0">
         <p className="text-center text-sm text-zinc-500 md:text-left">
-          © {new Date().getFullYear()} Hochu-portfolio. All rights reserved.
+          © {new Date().getFullYear()} Hochu-portfolio.
         </p>
-        <p className="text-center text-sm text-zinc-500 md:text-left">Crafted with precision by Hochu</p>
       </div>
     </footer>
   )
 }
-
