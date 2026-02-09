a4903b7938ed904d891334f931fdfc11d1b69fa8
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2026-02-10 01:29:21 +0900

    hobby非表示に

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Profile } from "@/components/profile"
 import { Experience } from "@/components/experience"
 import { Skills } from "@/components/skills"
 import { Projects } from "@/components/projects"
-import { Hobby } from "@/components/hobby"
+//import { Hobby } from "@/components/hobby"
 import { Contact } from "@/components/contact"
 import { Footer } from "@/components/footer"
 
@@ -18,7 +18,7 @@ export default function Home() {
         <Experience />
         <Skills />
         <Projects />
-        <Hobby />
+        {/* <Hobby /> */}
         <Contact />
       </main>
       <Footer />
